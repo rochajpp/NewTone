dc448da5ea9ebb2fef79a4ffb3f423eeba8cd231
João Paulo Rocha <joao.rochap03@gmail.com>
2024-11-27 10:39:47 -0300

    Finalizando o select

--- Server/app/public/home/home.js
+++ Server/app/public/home/home.js
@@ -4,37 +4,44 @@ function closeSelect(){
     const options = document.querySelector(".options");
     const optionsA = document.querySelectorAll(".options a");
 
+    optionsA.forEach((a, index) => {
+        a.style.top = "0";      
+     })
+   
+
+    options.style.top = "0";
+
     setTimeout(() => {
-        options.style.top = "0";
-    }, 1)
-
-    options.style.display = "none";
-
-    setTimeout(() => {
-        optionsA.forEach((a) => {
-            a.style.position = "absolute";
-        })
-    }, 1000);
-
+        options.style.display = "none";
+    }, 100);
+    
     select.onclick = openSelect;
 }
 
 function openSelect(){
     const options = document.querySelector(".options");
     const optionsA = document.querySelectorAll(".options a");
+
     options.style.display = "flex";
 
     setTimeout(() => {
         options.style.top = "3rem";
     }, 1)
 
-    setTimeout(() => {
-        optionsA.forEach((a) => {
-            a.style.position = "relative";
-        })
-    }, 1000);
+    
+
+    optionsA.forEach((a, index) => {
+       if(index != 0){
+            setTimeout(() => {
+                a.style.top = (index * 2.724) + "rem";
+            }, 90 * index);
+       }
+    });
+  
 
     select.onclick = closeSelect;
 }
 
 select.onclick = openSelect;
+
+
