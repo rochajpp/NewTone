5084752f5a4ca356e34f8e670cd9a849031f1977
João Paulo Rocha <joao.rochap03@gmail.com>
2024-12-12 14:07:23 -0300

    Organizando arquivos

--- Client/newtone/src/App.jsx
+++ Client/newtone/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
-import Home from "./pages/Home.jsx"
+import Home from "./pages/Home/Home.jsx"
 
 function App() {
   return (
